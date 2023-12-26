8d53a4a1c0ded0e9c8c07c6c4207892dc3ba5c2f
Mohamed Nour <alkhalafmuhamade@gmail.com>
2023-12-26 15:16:43 +0300

    ii changed the time passing console output to a more readable format

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -27,7 +27,7 @@ public class Main {
                 dispatcher.dispatch();
                 currentTime++;
                 // Optional: simulate time passing
-                System.out.print("aaa");
+                System.out.print("Press enter to continue...");
             }
         } catch (IOException e) {
             e.printStackTrace();
